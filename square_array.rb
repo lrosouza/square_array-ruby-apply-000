3db213675875517c780174311cb4cb363157d4c1
Lucas Souza <souza.lucas01@gmail.com>
2020-03-16 00:41:34 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,6 +2,6 @@ def square_array(array)
   # your code here
     array = [1,2,3]
     array.each do |square_array|
-    array = [square_array**2, square_array**2, square_array**2]
+    array << square_array**2
     end
 end
